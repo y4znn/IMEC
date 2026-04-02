d6cad9a0c59cbf823a94e0274d2e7b805540fde7
Ahmad Ghosn <ahmadghosn@Ahmads-MacBook-Air.local>
2026-04-03 00:20:40 +0300

    chore: add Google Analytics gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <head>
+                {/* Google tag (gtag.js) */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-LSVJ1S8EDX"></script>
+                <script dangerouslySetInnerHTML={{ __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-LSVJ1S8EDX');
+                ` }} />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <title>IMEC Radar</title>
